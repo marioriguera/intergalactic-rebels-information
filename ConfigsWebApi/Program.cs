6ab99ca0859a54c510d3dd16813fe876cdef9803
Mario David Riguera Castillo <mario.riguera@gmail.com>
2024-07-08 11:45:01 +0200

    wip: mediatr and stylecops nuggets was added.

--- ConfigsWebApi/Program.cs
+++ ConfigsWebApi/Program.cs
@@ -1,36 +1,40 @@
+namespace ConfigsWebApi;
 
-namespace ConfigsWebApi
+/// <summary>
+/// The main entry point for the application.
+/// </summary>
+public class Program
 {
-    public class Program
+    /// <summary>
+    /// The main method that starts the application.
+    /// </summary>
+    /// <param name="args">Command-line arguments.</param>
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllers();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            // if (app.Environment.IsDevelopment())
-            // {
             app.UseSwagger();
             app.UseSwaggerUI();
-            // }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
