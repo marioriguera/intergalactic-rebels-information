57db233755fa76f1fe6a9a55689e74f8475a9427
Mario David Riguera Castillo <mario.riguera@gmail.com>
2024-07-12 10:38:14 +0200

    wip: add summary to dependency injection class on application layer.

--- ConfigsApplication/DependencyInjection.cs
+++ ConfigsApplication/DependencyInjection.cs
@@ -4,8 +4,16 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace ConfigsApplication;
 
+/// <summary>
+/// Provides extension methods for adding application services to the service collection.
+/// </summary>
 public static class DependencyInjection
 {
+    /// <summary>
+    /// Adds application services to the specified <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The service collection to which the services are added.</param>
+    /// <returns>The service collection with the added services.</returns>
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(config =>
